c96e075c232919938d409b7b5b2977f62b43936f
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-11-06 16:18:06 +0100

    symptomReader done

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -11,32 +11,43 @@ public class AnalyticsCounter {
 
   public static void main(String[] args) throws Exception {
 
-    BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-    String line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
+      String line = reader.readLine();
 
-    int i = 0;
-    // int headCount = 0;
-    while (line != null) {
-      i += 1;
-      // System.out.println("symptom from file: " + line);
-      if (line.equals("headache")) {
-        headacheCount++;
-        System.out.println("number of headaches: " + headacheCount);
-      } else if (line.equals("rash")) {
-        rashCount++;
-        System.out.println("number of headaches: " + rashCount);
-      } else if (line.contains("pupils")) {
-        pupilCount++;
-        System.out.println("number of headaches: " + pupilCount);
+      // int headCount = 0;
+      while (line != null) {
+        // System.out.println("symptom from file: " + line);
+        if (line.equals("headache")) {
+          headacheCount++;
+          System.out.println("number of headaches: " + headacheCount);
+        } else if (line.equals("rash")) {
+          rashCount++;
+          System.out.println("number of rashs: " + rashCount);
+        } else if (line.contains("pupils")) {
+          pupilCount++;
+          System.out.println("number of pupils: " + pupilCount);
+        }
+
+        line = reader.readLine();
+
+        try (FileWriter writer = new FileWriter("result.out")) {
+          writer.write("headache: " + headacheCount + "\n");
+          writer.write("rash: " + rashCount + "\n");
+          writer.write("dialated pupils: " + pupilCount + "\n");
+        }
       }
-
-      line = reader.readLine();
-
-      FileWriter writer = new FileWriter("result.out");
-      writer.write("headache: " + headacheCount + "\n");
-      writer.write("rash: " + rashCount + "\n");
-      writer.write("dialated pupils: " + pupilCount + "\n");
-      writer.close();
+      reader.close();
     }
   }
 }
+// Lire les données provenant d'un fichier
+// @param => file
+//@result => list of symptom
+
+// analyser les données faire une liste des symptomes avec leur nombre associé
+// @param => list of symptom
+//@result => 
+
+// Trier les données et les écrire dans un fichier result.out
+// @param =>
+//@result =>
--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomReader.java
@@ -1,20 +1,57 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Anything that will read symptom data from a source
- * The important part is, the return value from the operation, which is a list of strings,
+ * The important part is, the return value from the operation, which is a list
+ * of strings,
  * that may contain many duplications
- * 
+ *
  * The implementation does not need to order the list
- * 
+ *
  */
 public interface ISymptomReader {
-	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
-	 */
-	List<String> GetSymptoms ();
+  /**
+   * transform a file into a list of symptoms
+   *
+   * @param a file with a list of symptoms as data source
+   *
+   * @return a raw listing of all Symptoms obtained from a data source, duplicates
+   *         are possible/probable
+   */
+  List<String> GetSymptoms(String filename);
+
+  /**
+   * get symptoms with associated number of times from a list of symptoms
+   *
+   * @param list of symptoms
+   *
+   * @return a collection of symptoms with their associated number of times
+   */
+  public Map<String, Integer> GetSymptomsWithAssociatedNumbers(List<String> symptoms);
+
+  /**
+   * write a file from map list of symptoms with associated number of times
+   *
+   * @param a collection of symptoms with their associated number of times
+   *
+   * @return a file with a sorted listed of symptoms with their associated number
+   *         of times
+   */
+  void writeSymptomsWithAssociatedNumbers(Map<String, Integer> mapSymptomsWithAssociatedNumbers) throws IOException;
 }
+
+// Lire les données provenant d'un fichier
+// @param => file
+// @result => list of symptom
+
+// analyser les données faire une liste des symptomes avec leur nombre associé
+// @param => list of symptom
+// @result =>
+
+// Trier les données et les écrire dans un fichier result.out
+// @param =>
+// @result =>
