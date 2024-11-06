90c6aecbdb763304cdd82b9c011b2266a3dc039c
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-11-06 17:32:06 +0100

    code review

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,42 +1,23 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class AnalyticsCounter {
-  private static int headacheCount = 0;
-  private static int rashCount = 0;
-  private static int pupilCount = 0;
 
-  public static void main(String[] args) throws Exception {
+  ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile();
 
-    try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
-      String line = reader.readLine();
-
-      // int headCount = 0;
-      while (line != null) {
-        // System.out.println("symptom from file: " + line);
-        if (line.equals("headache")) {
-          headacheCount++;
-          System.out.println("number of headaches: " + headacheCount);
-        } else if (line.equals("rash")) {
-          rashCount++;
-          System.out.println("number of rashs: " + rashCount);
-        } else if (line.contains("pupils")) {
-          pupilCount++;
-          System.out.println("number of pupils: " + pupilCount);
-        }
-
-        line = reader.readLine();
-
-        try (FileWriter writer = new FileWriter("result.out")) {
-          writer.write("headache: " + headacheCount + "\n");
-          writer.write("rash: " + rashCount + "\n");
-          writer.write("dialated pupils: " + pupilCount + "\n");
-        }
-      }
-      reader.close();
-    }
+  public List<String> reading(String file) {
+    return readSymptomDataFromFile.getSymptoms(file);
   }
+
+  public Map<String, Integer> analysing(List<String> symptoms) {
+    return readSymptomDataFromFile.getSymptomsWithAssociatedNumbers(symptoms);
+  }
+
+  public void writing(Map<String, Integer> mapSymptomsWithAssociatedNumbers) throws IOException {
+    readSymptomDataFromFile.writeSymptomsWithAssociatedNumbers(mapSymptomsWithAssociatedNumbers);
+  }
+
 }
